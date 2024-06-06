8d9007f9efcc26e50d2ef17e19f552e29e615c38
Diego Santana <3c.santana@gmail.com>
2024-06-06 06:16:52 -0300

    Iniciando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CharacterListComponent } from './pages/character-list/character-list.component';
+import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    CharacterListComponent,
+    CharacterDetailsComponent,
+    LoginComponent,
+    ProfileComponent,
+    SearchBarComponent,
+    CharacterCardComponent,
   ],
   imports: [
     BrowserModule,
