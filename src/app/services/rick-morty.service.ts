032f4e3d27bfc5dceadbbc09d2d0cfd8c05dfbaf
Diego Santana <3c.santana@gmail.com>
2024-06-06 12:13:58 -0300

    feat(services): Implementa a lógica para retornar todos os personagens

--- src/app/services/rick-morty.service.ts
+++ src/app/services/rick-morty.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { ICharacter } from '../models/character.interface';
+import { IApiResponse } from '../models/api-response.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RickMortyService {
+  baseUrl: string = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllCharacters(): Observable<IApiResponse<ICharacter>> {
+    return this.http.get<IApiResponse<ICharacter>>(`${this.baseUrl}/character`);
+  }
+
 }
