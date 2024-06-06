02f10336e06a6763c0480ebf3bc92594870c25f9
Diego Santana <3c.santana@gmail.com>
2024-06-06 11:17:33 -0300

    feat(components): Implementa a barra de pesquisa

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
+  lists: string[] = ['Buscar por:', 'Characters', 'Episodes', 'Locations'];
+  genders: string[] = ['female', 'male', 'genderless', 'unknown'];
+  status: string[] = ['alive', 'dead', 'unknown'];
+
+  selectedList: string = this.lists[0]; // Valor padrão
+  selectedGender: string = this.genders[0]; // Valor padrão
+  selectedStatus: string = this.status[0]; // Valor padrão
+
+  userInput: string = '';
+  showInput: boolean = false;
+
+  toggleInput(): void {
+    if(this.selectedList == this.lists[0]) {
+      this.showInput = false;
+      this.userInput = ''; // Limpa o input se voltar para o valor padrão
+    } else{
+      this.showInput = true;
+    }
+  }
+
+  onSearch(): void {
+    // pega a lista que o user selecionar
+    let searchParams = this.selectedList;
+
+    // aqui você vai criar as lógicas de busca
+    if (this.showInput && this.userInput.trim() !== '') {
+
+      searchParams += ` ${this.userInput.trim()}`;
+    }
+    // this.searchService.setSearchTerm(searchTerm);
+    console.log(this.userInput);
+    this.userInput = ''; // Limpa o input se voltar para o valor padrão
+  }
 
 }
