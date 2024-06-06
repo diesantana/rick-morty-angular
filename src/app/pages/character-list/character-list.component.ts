720c7f4b92e447432dff31a3ef8286c1061f7363
Diego Santana <3c.santana@gmail.com>
2024-06-06 12:16:31 -0300

    feat(pages): Implementa o método getAll no listComponent

--- src/app/pages/character-list/character-list.component.ts
+++ src/app/pages/character-list/character-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICharacter } from '../../models/character.interface';
+import { RickMortyService } from '../../services/rick-morty.service';
+import { IApiResponse } from '../../models/api-response.interface';
 
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
   styleUrl: './character-list.component.css'
 })
-export class CharacterListComponent {
+export class CharacterListComponent implements OnInit {
+  characters: ICharacter[] = [];
+
+  constructor(private rickMortyService: RickMortyService) {}
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this.rickMortyService.getAllCharacters().subscribe((data: IApiResponse<ICharacter>) => {
+      this.characters = data.results;
+    });
+  }
+
 
 }
