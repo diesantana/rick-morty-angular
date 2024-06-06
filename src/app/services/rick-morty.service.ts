4c1ad9bd4c84eca57aa2033faba1878858d7f8ea
Diego Santana <3c.santana@gmail.com>
2024-06-06 16:21:37 -0300

    feat(service): Implementa a busca com parâmetros

--- src/app/services/rick-morty.service.ts
+++ src/app/services/rick-morty.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { Observable } from 'rxjs';
 import { ICharacter } from '../models/character.interface';
 import { IApiResponse } from '../models/api-response.interface';
+import { ISearchParams } from '../models/search-params.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,19 @@ export class RickMortyService {
 
   constructor(private http: HttpClient) {}
 
-  getAllCharacters(): Observable<IApiResponse<ICharacter>> {
-    return this.http.get<IApiResponse<ICharacter>>(`${this.baseUrl}/character`);
+  getAllCharacters(params: ISearchParams): Observable<IApiResponse<ICharacter>> {
+    // Inicializa HttpParams vazio
+    let httpParams = new HttpParams();
+
+    // Itera sobre cada chave no objeto de parâmetros
+    for(const key in params) {
+      // Se o valor do parâmetro não for vazio, adicionamos ao HttpParams
+      if(params[key]) {
+        httpParams = httpParams.set(key, params[key]);
+      }
+    }
+    //Faz a requisição HTTP GET com os parâmetros (se houver)
+    return this.http.get<IApiResponse<ICharacter>>(`${this.baseUrl}/character/`, {params: httpParams});
   }
 
 }
