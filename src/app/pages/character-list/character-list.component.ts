af35435e9d41e0b3a6862b345d203a03b83fe326
Diego Santana <3c.santana@gmail.com>
2024-06-07 07:04:05 -0300

    feat(CharacterList.ts): Altera a lógica de carregamento de listagem inicial

--- src/app/pages/character-list/character-list.component.ts
+++ src/app/pages/character-list/character-list.component.ts
@@ -10,23 +10,48 @@ import { IApiResponse } from '../../models/api-response.interface';
 })
 export class CharacterListComponent implements OnInit {
   characters: ICharacter[] = [];
+  page: number = 1;
+  searchParams: any = {};
 
-  constructor(private rickMortyService: RickMortyService) {}
-
-  ngOnInit(): void {
-    this.getAll();
+  constructor(private rickMortyService: RickMortyService) {
   }
 
-  getAll(): void {
-    const params = {name: '', gender: '', status: ''};
+  ngOnInit(): void {
+    // A busca inicial será feita pelo SearchBarComponent
+  }
+
+  // Atualiza a lista de personagens com os resultados da pesquisa
+  handleSearchSuccess(data: IApiResponse<ICharacter>): void {
+    this.characters = data.results;
+    this.page = 1; // Reseta a página ao fazer uma nova busca
+  }
+
+  // Atualiza os parâmetros de busca e faz a requisição inicial
+  handleSearchParams(params: any): void {
+    this.searchParams = params;
+    this.page = 1; // Reseta a página ao fazer uma nova busca
+    this.loadCharacters();
+  }
+
+  // Carrega personagens com base nos parâmetros de busca e na página atual
+  loadCharacters(): void {
+    // Combina os parâmetros de busca atuais com a página atual usando o operador de espalhamento
+    const params = {...this.searchParams, page: this.page};
     this.rickMortyService.getAllCharacters(params).subscribe((data: IApiResponse<ICharacter>) => {
-      this.characters = data.results;
+      if(this.page === 1) {
+        this.characters = data.results;
+      } else {
+        this.characters = [...this.characters, ...data.results];
+      }
     });
   }
 
-  handleSearchSuccess(data: IApiResponse<ICharacter>): void {
-    this.characters = data.results; // Atualiza a lista de personagens com os resultados da pesquisa
+  // Incrementa a página e carrega mais personagens ao rolar a lista
+  onScroll() {
+    this.page++;
+    console.log("scrolled!!");
+    console.log(this.page);
+    this.loadCharacters();
   }
-
 
 }
