05aee27d47f27e43fc8d68086c9f030e03e413e1
Diego Santana <3c.santana@gmail.com>
2024-06-07 20:17:47 -0300

    faet(episode-table): Cria a tabela

---
+++ src/app/components/episode-table/episode-table.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input } from '@angular/core';
+import { IEpisode } from '../../models/episode.interface';
+import { RickMortyService } from '../../services/rick-morty.service';
+import { IApiResponse } from '../../models/api-response.interface';
+
+@Component({
+  selector: 'app-episode-table',
+  templateUrl: './episode-table.component.html',
+  styleUrl: './episode-table.component.css'
+})
+export class EpisodeTableComponent {
+  @Input() episodes: IEpisode[] = [];
+  @Input() nextPage!: string | null;
+  @Input() prevPage!: string | null;
+  loading: boolean = false;
+
+  constructor(private rickMortyService: RickMortyService) {}
+
+  ngOnInit(): void {
+  }
+
+  onNextPage(): void {
+    this.loading = true;
+    if(this.nextPage) {
+      this.rickMortyService.getNextEpisodes(this.nextPage).subscribe((data: IApiResponse<IEpisode>) => {
+        this.episodes = data.results;
+        this.nextPage = data.info.next;
+        this.prevPage = data.info.prev;
+        this.loading = false;
+      });
+    }
+  }
+
+  onPrevPage(): void {
+    this.loading = true;
+    if(this.prevPage) {
+      this.rickMortyService.getNextEpisodes(this.prevPage).subscribe((data: IApiResponse<IEpisode>) => {
+        this.episodes = data.results;
+        this.nextPage = data.info.next;
+        this.prevPage = data.info.prev;
+        this.loading = false;
+      });
+    }
+  }
+
+}
