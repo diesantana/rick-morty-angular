3a29f5532ab850592f13e17146c0349489d9c086
Diego Santana <3c.santana@gmail.com>
2024-06-07 20:31:29 -0300

    feat(episode-list): Lógica da para carregar os dados na lista

---
+++ src/app/pages/episode-list/episode-list.component.ts
@@ -0,0 +1,66 @@
+import { Component, Input } from '@angular/core';
+import { IEpisode } from '../../models/episode.interface';
+import { RickMortyService } from '../../services/rick-morty.service';
+import { IApiResponse } from '../../models/api-response.interface';
+
+@Component({
+  selector: 'app-episode-list',
+  templateUrl: './episode-list.component.html',
+  styleUrl: './episode-list.component.css'
+})
+export class EpisodeListComponent {
+  currentList: string = 'Episodes';
+  episodes: IEpisode[] = [];
+  page: number = 1;
+  nextPage!: string | null;
+  prevPage!: string | null;
+
+  constructor(private rickMortyService: RickMortyService) {
+  }
+
+  ngOnInit(): void {
+    this.loadEpisodes();
+  }
+
+ // Atualiza a lista com os resultados de busca
+  handleReponseSuccess(data: any): void {
+    if(data.info) {
+      this.episodes = data.results;
+      this.nextPage = data.info.next;
+      this.prevPage = data.info.prev;
+    } else {
+      this.episodes = [];
+      this.episodes.push(data);
+    }
+  }
+
+  // Carrega personagens com base nos parâmetros de busca e na página atual
+  loadEpisodes(): void {
+    this.rickMortyService.getAllEpisodes("").subscribe((data: IApiResponse<IEpisode>) => {
+      this.episodes = data.results;
+      this.nextPage = data.info.next;
+      this.prevPage = data.info.prev;
+    });
+  }
+
+  onNextPage() {
+    if(this.nextPage) {
+      this.rickMortyService.getNextEpisodes(this.nextPage).subscribe((data: IApiResponse<IEpisode>) => {
+        this.episodes = data.results;
+        this.nextPage = data.info.next;
+        this.prevPage = data.info.prev;
+      })
+    }
+  }
+
+  onPrevPage() {
+    if(this.prevPage) {
+      this.rickMortyService.getNextEpisodes(this.prevPage).subscribe((data: IApiResponse<IEpisode>) => {
+        this.episodes = data.results;
+        this.nextPage = data.info.next;
+        this.prevPage = data.info.prev;
+      })
+    }
+  }
+
+}
