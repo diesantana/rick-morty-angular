29df6be61729271bdd845426786c5d2f963783f7
Diego Santana <3c.santana@gmail.com>
2024-06-07 20:32:24 -0300

    feat(character-service): Lógica para armazenar um character clicado

---
+++ src/app/services/character.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ICharacter } from '../models/character.interface';
+/*
+  serviço para armazenar os detalhes do personagem
+*/
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+  // BehaviorSubject para armazenar os detalhes do personagem
+  private characterDetails = new BehaviorSubject<ICharacter | undefined>(undefined);
+  // Observable para fornecer acesso aos detalhes do personagem
+  characterDetails$ = this.characterDetails.asObservable();
+
+  constructor() { }
+
+  // Método para definir os detalhes do personagem
+  setCharacterDetails(character: ICharacter): void {
+    this.characterDetails.next(character);
+  }
+
+  // Método para limpar os detalhes do personagem
+  clearCharacterDetails(): void {
+    this.characterDetails.next(undefined);
+  }
+
+}
