80880b567dc1fb3db205d7c5c6b0454f0f83278b
Diego Santana <3c.santana@gmail.com>
2024-06-08 19:09:06 -0300

    feat(app-routing.module): Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CharacterListComponent } from './pages/character-list/character-list.component';
 import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -8,8 +7,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { EpisodeListComponent } from './pages/episode-list/episode-list.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: '/characters', pathMatch: 'full'},
   {path: 'characters', component: CharacterListComponent},
   {path: 'characters/:id', component: CharacterDetailsComponent},
   {path: 'episodes', component: EpisodeListComponent},
