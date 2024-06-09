31248070ad7ea15ced2a59e58a7b48bcd7c56419
Diego Santana <3c.santana@gmail.com>
2024-06-09 11:04:30 -0300

    feat(header): Lógica para o login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../../services/session.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  user: any = null;
+
+  constructor(private sessionService: SessionService) {}
+
+  ngOnInit(): void {
+    this.sessionService.user$.subscribe( user => {
+      this.user = user;
+    })
+  }
+
+  logout(): void {
+    this.sessionService.clearUser();
+  }
+
+  get isLoggedIn(): boolean {
+
+    return this.user !== null;
+  }
 
 }
