0e7780b4ae3c669a58709b55cece0025c31e4658
Diego Santana <3c.santana@gmail.com>
2024-06-09 06:27:41 -0300

    feat(profile): Cria a tela de perfil

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  dataDashboard: {data: string, value: number} [] = [
+    {data: 'Personagens encontrados', value: 826},
+    {data: 'Episódios encontrados', value: 51},
+    {data: 'Localizações: ', value: 126},
+  ];
 
 }
